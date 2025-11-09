34f05923968147061e50a6a9e42724ce38035072
teralani <aniketh.terala@gmail.com>
2025-11-08 19:54:29 -0800

    s

--- iyna_website/src/app/components/quote.tsx
+++ iyna_website/src/app/components/quote.tsx
@@ -24,7 +24,7 @@ const Quote = ({children, author} : {children : ReactNode, author: string}) => {
                     <div id="t" className="py-4"> <span className="text-lg md:text-2xl font-bold">— {author}</span> <p className="relative mx-7 italic">Chapter President</p></div>
                     <svg  className="float-right -my-22 w-20 fill-blue" clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m2.699 20c-.411 0-.699-.312-.699-.662 0-.249.145-.516.497-.703 1.788-.947 3.858-4.226 3.858-6.248-3.016.092-4.326-2.582-4.326-4.258 0-2.006 1.738-4.129 4.308-4.129 3.241 0 4.83 2.547 4.83 5.307 0 5.981-6.834 10.693-8.468 10.693zm10.833 0c-.41 0-.699-.312-.699-.662 0-.249.145-.516.497-.703 1.788-.947 3.858-4.226 3.858-6.248-3.015.092-4.326-2.582-4.326-4.258 0-2.006 1.739-4.129 4.308-4.129 3.241 0 4.83 2.547 4.83 5.307 0 5.981-6.833 10.693-8.468 10.693z" fillRule="nonzero"/></svg>
                 </span>
-            </div>
+            </div> 
             <style jsx>{`
                 @media screen and (min-width: 1058px ) {
                     .quote::after {
